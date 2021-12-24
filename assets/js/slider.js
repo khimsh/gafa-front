84d571d85512aca65e83acfb1f621832bfdd74a1
khimsh <i.khimsh@gmail.com>
2021-12-24 22:39:08 +0400

    added some of responsive styles

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -45,13 +45,6 @@ $(document).ready(function () {
     nextArrow: nextArrow,
     responsive: [
       {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
         breakpoint: 768,
         settings: {
           slidesToShow: 2,
